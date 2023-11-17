3beba6d74f0e749abf4c5a921252c550a7d43279
Madhubhashana Mahindarathne <104136158+Madhubhashana99@users.noreply.github.com>
2023-11-17 15:05:37 +0530

    update

--- Project_Front/src/app/app-routing.module.ts
+++ Project_Front/src/app/app-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 
 const routes: Routes = [
 
   
   {path:"",component:LoginComponent},
-  {path:"register",component:RegisterComponent}
+  {path:"register",component:RegisterComponent},
+  {path:"userdash",component:UserDashboardComponent},
+  {path:"admindash",component:AdminDashboardComponent}
 ];
 
 @NgModule({
